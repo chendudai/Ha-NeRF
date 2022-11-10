a2d5862dcefe1b68214ae7f2befef6ae7537f3bb
chendudai <chendudai10@gmail.com>
2022-11-10 14:59:18 +0200

    docker try

---
+++ clone_repo_and_run_python_scripts.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/chendudai/Ha-NeRF
+cd /pytorch-tutorial/tutorials/01-basics/feedforward_neural_network
+
+python main.py
+python test_script.py
+python finalize_script.py
